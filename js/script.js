15bf161fe2215c79f0e6db0f0241cdc96409162e
x <x@x.com>
2015-09-11 10:19:00 -0700

    10-18-AM

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function set_event_listeners(element_array,event_type){
 	for(i=0;i<element_array.length;i++){
 		element_array[i].addEventListener(event_type,function(){
 
-			alert(this);
+			this.style.backgroundColor = '#fafafa'
 		});
 	}
 };
