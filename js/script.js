2b25b6087a726aa5ac3cccedadd4875c437676fb
x <x@x.com>
2015-09-11 11:10:56 -0700

    11-10-AM

--- js/script.js
+++ js/script.js
@@ -5,45 +5,48 @@ function update_element_array(element_tag_name){
 	return element_array;
 }
 
-function detectswipe(el,func) {
-	swipe_det = new Object();
-	swipe_det.sX = 0;
-	swipe_det.sY = 0;
-	swipe_det.eX = 0;
-	swipe_det.eY = 0;
-	var min_x = 25;  //min x swipe for horizontal swipe
-	var max_x = 40;  //max x difference for vertical swipe
-	var min_y = 40;  //min y swipe for vertical swipe
-	var max_y = 60;  //max y difference for horizontal swipe
-	var direc = "";
-	ele = document.getElementById(el);
-	ele.addEventListener('touchstart',function(e){
-		var t = e.touches[0];
-		swipe_det.sX = t.screenX;
-		swipe_det.sY = t.screenY;
+function detect_swipe_event(element_id_name,name_of_function_trigger_from_touch_event) {
+	var class_swipe = new Object();
+	class_swipe.direction_x_start = 0;
+	class_swipe.direction_y_start = 0;
+	class_swipe.direction_x_end = 0;
+	class_swipe.direction_y_end = 0;
+	var touch_direction_x_minimum = 20;  //min x swipe for horizontal swipe
+	var touch_direction_x_maximum = 40;  //vertical swipe
+	var touch_direction_y_minimum = 40;  //vertical swipe
+	var touch_direction_y_maximum = 50;  //horizontal swipe
+	var touch_direction = "";
+	var current_element = document.getElementById(element_id_name);
+
+	current_element.addEventListener('touchstart',function(event){
+		var current_target = event.touches[0];
+		class_swipe.direction_x_start = current_target.screenX;
+		class_swipe.direction_y_start = current_target.screenY;
 	},false);
-	ele.addEventListener('touchmove',function(e){
-		e.preventDefault();
-		var t = e.touches[0];
-		swipe_det.eX = t.screenX;
-		swipe_det.eY = t.screenY;
+
+	current_element.addEventListener('touchmove',function(event){
+		event.preventDefault();
+		var current_target = event.touches[0];
+		class_swipe.direction_x_end = current_target.screenX;
+		class_swipe.direction_y_end = current_target.screenY;
 	},false);
-	ele.addEventListener('touchend',function(e){
+
+	current_element.addEventListener('touchend',function(event){
 		//horizontal detection
-		if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y)))) {
-			if(swipe_det.eX > swipe_det.sX) direc = "r";
-			else direc = "l";
+		if ((((class_swipe.direction_x_end - touch_direction_x_minimum > class_swipe.direction_x_start) || (class_swipe.direction_x_end + touch_direction_x_minimum < class_swipe.direction_x_start)) && ((class_swipe.direction_y_end < class_swipe.direction_y_start + touch_direction_y_maximum) && (class_swipe.direction_y_start > class_swipe.direction_y_end - touch_direction_y_maximum)))) {
+			if(class_swipe.direction_x_end > class_swipe.direction_x_start) touch_direction = "right";
+			else touch_direction = "left";
 		}
 		//vertical detection
-		if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x)))) {
-			if(swipe_det.eY > swipe_det.sY) direc = "d";
-			else direc = "u";
+		if ((((class_swipe.direction_y_end - touch_direction_y_minimum > class_swipe.direction_y_start) || (class_swipe.direction_y_end + touch_direction_y_minimum < class_swipe.direction_y_start)) && ((class_swipe.direction_x_end < class_swipe.direction_x_start + touch_direction_x_maximum) && (class_swipe.direction_x_start > class_swipe.direction_x_end - touch_direction_x_maximum)))) {
+			if(class_swipe.direction_y_end > class_swipe.direction_y_start) touch_direction = "down";
+			else touch_direction = "up";
 		}
 
-		if (direc != "") {
-			if(typeof func == 'function') func(el,direc);
+		if (touch_direction != "") {
+			if(typeof name_of_function_trigger_from_touch_event == 'function') name_of_function_trigger_from_touch_event(element_id_name,touch_direction);
 		}
-		direc = "";
+		touch_direction = "";
 	},false);
 }
 
@@ -88,9 +91,9 @@ window.onload = function(){
 	//set_event_listeners(list_items,'click');
 	var h=function(e){console.log(e.type,e)};
 
-	detectswipe('chess',set_random_color_for_list_item('chess'));
-	detectswipe('poker',set_random_color_for_list_item('poker'));
-	detectswipe('scrabble',set_random_color_for_list_item('scrabble'));
+	detect_swipe_event('chess',set_random_color_for_list_item('chess'));
+	detect_swipe_event('poker',set_random_color_for_list_item('poker'));
+	detect_swipe_event('scrabble',set_random_color_for_list_item('scrabble'));
 
 
 };
